7a4e929b57de88eef3789dc05bee6528eb3ac2e8
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-11-21 13:17:22 -0300

    Add test path

---
+++ src/main/java/br/com/aurora/backendbff/controllers/MainController.java
@@ -0,0 +1,21 @@
+package br.com.aurora.backendbff.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(
+    path = "/",
+    consumes = MediaType.APPLICATION_JSON_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE
+)
+public class MainController {
+    
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("Work");
+    }
+}
