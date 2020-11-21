a814470ba7145fb08b0cecfdf58e6bb5ff8c981a
Mariane Muniz <mariane.muniz.ribeiro@hotmail.com>
2020-11-21 13:04:22 -0300

    first commit

---
+++ src/main/java/br/com/aurora/backendbff/BackendBffApplication.java
@@ -0,0 +1,13 @@
+package br.com.aurora.backendbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aurora/backendbff/BackendBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aurora.backendbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
